64a68422d9ed1bce36e510a6a4123891c619f21f
taraanwar <taraanwar18@gmail.com>
2026-03-10 01:27:04 +0700

    [GREEN] Implement BankTransferPayment

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/BankTransferPayment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/BankTransferPayment.java
@@ -7,11 +7,23 @@ import java.util.Map;
 @Getter
 public class BankTransferPayment {
 
+    private static final String PENDING = "PENDING";
+    private static final String REJECTED = "REJECTED";
+
     String status;
     Map<String, String> paymentData;
 
     public BankTransferPayment(Map<String, String> paymentData) {
         this.paymentData = paymentData;
-    }
 
+        String bankName = paymentData.get("bankName");
+        String referenceCode = paymentData.get("referenceCode");
+
+        if (bankName != null && !bankName.isEmpty()
+                && referenceCode != null && !referenceCode.isEmpty()) {
+            this.status = PENDING;
+        } else {
+            this.status = REJECTED;
+        }
+    }
 }
