3ba11abc94e0f7886feeea50a083ea23dc947480
taraanwar <taraanwar18@gmail.com>
2026-03-10 01:17:15 +0700

    [GREEN] Implement VoucherCodePayment

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/VoucherCodePayment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/VoucherCodePayment.java
@@ -7,11 +7,32 @@ import java.util.Map;
 @Getter
 public class VoucherCodePayment {
 
+    private static final String SUCCESS = "SUCCESS";
+    private static final String REJECTED = "REJECTED";
+
     String status;
     Map<String, String> paymentData;
 
     public VoucherCodePayment(Map<String, String> paymentData) {
         this.paymentData = paymentData;
+
+        String voucherCode = paymentData.get("voucherCode");
+        if (voucherCode != null && voucherCode.length() == 16 && voucherCode.startsWith("ESHOP")) {
+            int digitCount = 0;
+            for (char c : voucherCode.toCharArray()) {
+                if (Character.isDigit(c)) {
+                    digitCount += 1;
+                }
+            }
+
+            if (digitCount == 8) {
+                this.status = SUCCESS;
+            } else {
+                this.status = REJECTED;
+            }
+        } else {
+            this.status = REJECTED;
+        }
     }
 
 }
